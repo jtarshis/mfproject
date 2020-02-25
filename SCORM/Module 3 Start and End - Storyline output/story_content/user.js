3a58bef50a6ccf7ebb2b95564a7203014e0cb6a3
jtarshis <jeremytarshis@yahoo.com>
2020-02-25 16:43:33 -0700

    Update module 3

---
+++ SCORM/Module 3 Start and End - Storyline output/story_content/user.js
@@ -0,0 +1,24 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "5pRw6H0MpEq":
+        Script1();
+        break;
+  }
+}
+
+function Script1()
+{
+  var player = GetPlayer();
+var user_15_submission = player.GetVar("user_15_submission");
+var user_15_submission = user_15_submission + " "; 
+var initial_whitespace_rExp = /^[^A-Za-z0-9.']+/gi;
+var left_trimmedStr = user_15_submission.replace(initial_whitespace_rExp, "");
+var non_alphanumerics_rExp = rExp = /[^A-Za-z0-9.']+/gi;
+var cleanedStr = left_trimmedStr.replace(non_alphanumerics_rExp, " ");
+var splitString = cleanedStr.split(" ");
+var user_15_wordcount = splitString.length -1;
+player.SetVar("user_15_wordcount", user_15_wordcount);
+}
+
